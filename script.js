144f3844f553a4396d365e31ac610b8b2a6a2c06
Ahmad-U19 <ahmedoppal2592@gmail.com>
2024-11-05 22:04:49 +0500

    Final Code

---
+++ script.js
@@ -0,0 +1,29 @@
+var x="Uppal";
+x="Ahmad";
+x=3;
+
+let y="Hassaan";
+y="Sanwal";
+
+const z="Ahmad Uppal";
+document.write("<br>");
+document.write(typeof z);
+document.write("<br>");
+document.write(x+7);
+document.write("<br>");
+document.write(x-1);
+document.write("<br>");
+document.write(x*2);
+document.write("<br>");
+document.write(x**4);
+document.write("<br>");
+document.write(x%2);
+document.write("<br>");
+document.write(x+=3);
+document.write("<br>");
+document.write(x*=7);
+document.write("<br>");
+
+console.log(x);
+console.table("{1, 2, 3}");
+//console.clear();
